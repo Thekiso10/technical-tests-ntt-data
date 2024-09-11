6efb8d160ba6fd8c8573f1c01e9a52ce8b9fffd0
Thekiso <diogoassuncao@msn.com>
2024-09-11 10:57:10 +0200

    Swagger: Add documentation

--- src/main/java/com/ntt_data/inditex/backend/infrastracture/adapter/rest/PricesController.java
+++ src/main/java/com/ntt_data/inditex/backend/infrastracture/adapter/rest/PricesController.java
@@ -2,6 +2,11 @@ package com.ntt_data.inditex.backend.infrastracture.adapter.rest;
 
 import com.ntt_data.inditex.backend.application.usercase.GetAvailablePriceUseCase;
 import com.ntt_data.inditex.backend.domain.model.Prices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +30,12 @@ public class PricesController {
     private final GetAvailablePriceUseCase getAvailablePriceUseCase;
 
     @GetMapping("/available")
+    @Operation(summary = "Get valid price", description = "Get the valid price of a donated product on a given date", tags = {"Price"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns the correct record", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Prices.class))),
+            @ApiResponse(responseCode = "400", description = "Error in request parameters", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content(mediaType = "application/json"))
+    })
     public ResponseEntity<Prices> getAvailablePriceByProductAndBrandAndDate(
             @RequestParam @NotNull Long productId,
             @RequestParam @NotNull Long brandId,
